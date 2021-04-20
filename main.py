de1dbe26cd2b4f0b7e16a33540a1096d4a368ea5
neternefer <nodzynska@gmail.com>
2021-04-20 20:26:55 +0200

    add problems 3 - 5

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from fastapi import FastAPI, Request, Response, status
-from passlib.context import CryptContext
+from passlib.hash import sha512_crypt
 from typing import Dict
 from pydantic import BaseModel
-from datetime import date
+from datetime import date, timedelta
+from typing import Optional
 
 
 class Patient(BaseModel):
+    id: Optional[int] = 1
     name: str
-    surename: str
-    register_date: date
-    vaccination_date: date
+    surname: str
+    register_date: Optional[date]
+    vaccination_date: Optional[date]
 
 
 app = FastAPI()
-app.counter: int = 0
+app.counter: int = 1
 app.storage: Dict[int, Patient] = {}
 
 
@@ -25,33 +27,34 @@ def root():
 def read_request(request: Request, response: Response):
     if request.method == "POST":
         response.status_code = status.HTTP_201_CREATED
-
     return {"method": request.method}
 
+@app.get("/auth")
+def check_pass(password, password_hash, response: Response):
+    hashed = sha512_crypt.hash(password)
+    if password_hash == hashed:
+        response.status_code = status.HTTP_204_NO_CONTENT
+    else:
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+
+
+
 @app.post("/register")
-def new_patient(patient: Patient, request: Request, response: Response):
-    length = len(patient.name) + len(patient.surename)
-    reg_date = date
-    vac_date = reg_date + length
-    resp = {
-    "id": app.counter,
-    "name": patient.name,
-    "surname": patient.surename,
-    "register_date": reg_date,
-    "vaccination_date": vac_date
-}
+async def new_patient(patient: Patient, response: Response):
+    length = len(patient.name) + len(patient.surname)
+    patient.vaccination_date = patient.register_date + timedelta(days=length)
+    patient.id = app.counter
     app.storage[app.counter] = patient
     app.counter += 1
     response.status_code = status.HTTP_201_CREATED
-    return resp
+    return patient
 
 @app.get("/patient/{id}")
 def show_patient(id: int, response: Response):
     if id in app.storage:
-        if id > 0:
-            return app.storage.get(id)
-        else:
+        if id < 1:
             response.status_code = status.HTTP_400_BAD_REQUEST
+        return app.storage.get(id)
     response.status_code = status.HTTP_404_NOT_FOUND
 
 
