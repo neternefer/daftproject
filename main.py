548ede865a0109fa6b6ed8dd64e93cc6d777c7f8
Anna Nodzyńska <nodzynska@gmail.com>
2021-04-20 17:00:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 from fastapi import FastAPI, Request, Response, status
-from passlib.context import CryptContext
-from typing import Dict
-from pydantic import BaseModel
-from datetime import date
 
 
-class Patient(BaseModel):
-    name: str
-    surename: str
-    register_date: date
-    vaccination_date: date
 
 
 app = FastAPI()
 app.counter: int = 0
-app.storage: Dict[int, Patient] = {}
 
 
 @app.get("/")
@@ -27,31 +17,4 @@ def read_request(request: Request, response: Response):
         response.status_code = status.HTTP_201_CREATED
 
     return {"method": request.method}
-
-@app.post("/register")
-def new_patient(patient: Patient, request: Request, response: Response):
-    length = len(patient.name) + len(patient.surename)
-    reg_date = date
-    vac_date = reg_date + length
-    resp = {
-    "id": app.counter,
-    "name": patient.name,
-    "surname": patient.surename,
-    "register_date": reg_date,
-    "vaccination_date": vac_date
-}
-    app.storage[app.counter] = patient
-    app.counter += 1
-    response.status_code = status.HTTP_201_CREATED
-    return resp
-
-@app.get("/patient/{id}")
-def show_patient(id: int, response: Response):
-    if id in app.storage:
-        if id > 0:
-            return app.storage.get(id)
-        else:
-            response.status_code = status.HTTP_400_BAD_REQUEST
-    response.status_code = status.HTTP_404_NOT_FOUND
-
 
